523a375b39f1ade0dd84f0b5a4e682f07aa432f5
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2022-11-04 16:59:58 -0500

    fix(treatments): deleted unnecessary imports

--- src/main/java/com/theraphy/backendtheraphy/treatments/mapping/MappingConfiguration.java
+++ src/main/java/com/theraphy/backendtheraphy/treatments/mapping/MappingConfiguration.java
@@ -1,8 +1,5 @@
 package com.theraphy.backendtheraphy.treatments.mapping;
 
-import com.theraphy.backendtheraphy.security.mapping.PatientMapper;
-import com.theraphy.backendtheraphy.security.mapping.PhysiotherapistMapper;
-import com.theraphy.backendtheraphy.security.mapping.UserMapper;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
