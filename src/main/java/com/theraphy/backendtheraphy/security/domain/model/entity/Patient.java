5ea95b407a3e97350828e77343bd9599c07f4d4b
Luis Pineda <u20201e705@upc.edu.pe>
2022-11-09 18:17:52 -0500

    feat(review): added review's relationship in patient entity

--- src/main/java/com/theraphy/backendtheraphy/security/domain/model/entity/Patient.java
+++ src/main/java/com/theraphy/backendtheraphy/security/domain/model/entity/Patient.java
@@ -3,6 +3,7 @@ package com.theraphy.backendtheraphy.security.domain.model.entity;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.theraphy.backendtheraphy.shared.domain.model.AuditModel;
+import com.theraphy.backendtheraphy.social.domain.model.entity.Review;
 import lombok.*;
 
 import javax.persistence.*;
@@ -10,6 +11,8 @@ import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
 
 
 @Getter
@@ -48,4 +51,33 @@ public class Patient {
     @NotBlank
     @Column(name = "birthday_date")
     private String birthdayDate;
+
+
+
+    //relation
+
+    @OneToMany(cascade = CascadeType.ALL,
+            fetch = FetchType.EAGER, mappedBy = "patient")
+    private Set<Review> reviews = new HashSet<>();
+
+    public Patient addReview(String physiotherapist,
+                             String reviewer,
+                             Integer stars,
+                             String description,
+                             Physiotherapist doctor) {
+        // Initialize if null
+        if(reviews == null) {
+            reviews = new HashSet<>();
+        }
+
+        // Add Criterion to Skill
+        reviews.add(new Review()
+                        .withDescription(description)
+                        .withReviewer(reviewer)
+                        .withStars(stars)
+                        .withPhysiotherapist(physiotherapist)
+                        .withDoctor(doctor)
+                        .withPatient(this));
+        return this;
+    }
 }
