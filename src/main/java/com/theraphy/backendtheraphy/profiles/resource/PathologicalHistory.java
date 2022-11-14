826842e412ae2b6be553d5726ffd817cbd3780fa
CristhianGomez <u202020341@upc.edu.pe>
2022-11-14 17:49:15 -0500

    feat(medical-histories): modified PathologicalHistory entity for medical-history

---
+++ src/main/java/com/theraphy/backendtheraphy/profiles/resource/PathologicalHistory.java
@@ -0,0 +1,14 @@
+package com.theraphy.backendtheraphy.profiles.resource;
+
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class PathologicalHistory {
+    private Long id;
+    private String name;
+}
