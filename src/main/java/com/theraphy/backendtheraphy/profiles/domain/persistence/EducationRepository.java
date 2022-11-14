1b8b657746dbe14d6bd78d4b350df4f74797062f
alumnos <u20201a836@upc.edu.pe>
2022-11-14 18:36:48 -0500

    feat(educations): added repository for education

--- src/main/java/com/theraphy/backendtheraphy/profiles/domain/persistence/EducationRepository.java
+++ src/main/java/com/theraphy/backendtheraphy/profiles/domain/persistence/EducationRepository.java
@@ -1,4 +1,11 @@
 package com.theraphy.backendtheraphy.profiles.domain.persistence;
 
-public class EducationRepository {
+import com.theraphy.backendtheraphy.profiles.domain.model.entity.Education;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface EducationRepository extends JpaRepository<Education, Long> {
+
+    Education findBySpecialization(String specialization);
 }
