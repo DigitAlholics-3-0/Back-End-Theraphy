410ea5117dcede7136608cc7ba4186b7a0b648b9
CristhianGomez <u202020341@upc.edu.pe>
2022-11-14 17:48:11 -0500

    feat(medical-histories): modified MedicalHistory resource for medical-history

--- src/main/java/com/theraphy/backendtheraphy/profiles/resource/CreateMedicalHistoryResource.java
+++ src/main/java/com/theraphy/backendtheraphy/profiles/resource/CreateMedicalHistoryResource.java
@@ -1,4 +1,30 @@
 package com.theraphy.backendtheraphy.profiles.resource;
 
+import lombok.*;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
 public class CreateMedicalHistoryResource {
+
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
+    private String patientName;
+
+    @NotNull
+    private Double height;
+
+    @NotNull
+    private Double weight;
+
+    @NotNull
+    private Double bodyMass;
+
 }
--- src/main/java/com/theraphy/backendtheraphy/profiles/resource/UpdateMedicalHistoryResource.java
+++ src/main/java/com/theraphy/backendtheraphy/profiles/resource/UpdateMedicalHistoryResource.java
@@ -1,4 +1,28 @@
 package com.theraphy.backendtheraphy.profiles.resource;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
 public class UpdateMedicalHistoryResource {
+
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
+    private String patientName;
+
+    @NotNull
+    private Double height;
+
+    @NotNull
+    private Double weight;
+
+    @NotNull
+    private Double bodyMass;
+
 }
