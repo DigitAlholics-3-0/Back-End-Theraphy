9cccb94bceb1de939a9c627562b17d2aa3823226
CristhianGomez <u202020341@upc.edu.pe>
2022-11-14 17:46:33 -0500

    feat(medical-histories): modified Allergy resources for medical-history

---
+++ src/main/java/com/theraphy/backendtheraphy/profiles/resource/AllergyResource.java
@@ -0,0 +1,13 @@
+package com.theraphy.backendtheraphy.profiles.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class AllergyResource {
+    private Long id;
+    private String name;
+}
---
+++ src/main/java/com/theraphy/backendtheraphy/profiles/resource/UpdateAllergyResource.java
@@ -0,0 +1,17 @@
+package com.theraphy.backendtheraphy.profiles.resource;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+@Getter
+@Setter
+public class UpdateAllergyResource {
+    @NotNull
+    @NotBlank
+    @Size(max = 20)
+    private String allergyName;
+}
