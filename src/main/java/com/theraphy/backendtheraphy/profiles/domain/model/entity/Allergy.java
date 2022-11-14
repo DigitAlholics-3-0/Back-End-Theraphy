fc9cddf7caed6c5bca2a40ad5d1665f1dc56ca2c
CristhianGomez <u202020341@upc.edu.pe>
2022-11-14 17:46:06 -0500

    feat(medical-histories): modified Allergy entity for medical-history

--- src/main/java/com/theraphy/backendtheraphy/profiles/domain/model/entity/Allergy.java
+++ src/main/java/com/theraphy/backendtheraphy/profiles/domain/model/entity/Allergy.java
@@ -4,6 +4,9 @@ import com.theraphy.backendtheraphy.shared.domain.model.AuditModel;
 import lombok.*;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotBlank;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
 
 @Getter
 @Setter
@@ -18,6 +21,9 @@ public class Allergy{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @NotBlank
+    @Size(max = 200)
     @Column(length = 20)
     private String name;
 
