509e50e5c452ebceb1886991b5009404262363a3
GabrielGomezDlc <u202020341@upc.edu.pe>
2022-11-16 18:28:18 -0500

    feat(treatments): added services for treatments bound context

--- src/main/java/com/theraphy/backendtheraphy/treatments/domain/service/TreatmentService.java
+++ src/main/java/com/theraphy/backendtheraphy/treatments/domain/service/TreatmentService.java
@@ -1,10 +1,13 @@
 package com.theraphy.backendtheraphy.treatments.domain.service;
 
+import com.theraphy.backendtheraphy.profile.domain.model.entity.Physiotherapist;
+import com.theraphy.backendtheraphy.social.domain.model.entity.Review;
 import com.theraphy.backendtheraphy.treatments.domain.model.entity.Treatment;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.ResponseEntity;
 
+import java.util.Date;
 import java.util.List;
 
 public interface TreatmentService {
@@ -14,4 +17,10 @@ public interface TreatmentService {
     Treatment create(Treatment treatment);
     Treatment update(Long treatmentId, Treatment request);
     ResponseEntity<?> delete(Long treatmentId);
+
+    Treatment getByTitleAndPhysiotherapistId(String title, Long physiotherapistId);
+
+
+    Treatment addPatientToTreatment(Long treatmentI, Date registrationDate, Double progress);
+
 }
