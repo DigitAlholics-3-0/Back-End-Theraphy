1a6de77bda8e2aae50e646db35e6d708ffbb8ac4
MariaAhuanari <u20201a836@upc.edu.pe>
2022-11-19 19:54:36 -0500

    feat(appointments): modified repository for physiotherapist

--- src/main/java/com/theraphy/backendtheraphy/profile/domain/persistence/PhysiotherapistRepository.java
+++ src/main/java/com/theraphy/backendtheraphy/profile/domain/persistence/PhysiotherapistRepository.java
@@ -4,7 +4,10 @@ import com.theraphy.backendtheraphy.profile.domain.model.entity.Physiotherapist;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PhysiotherapistRepository extends JpaRepository<Physiotherapist, Long> {
     Physiotherapist findByFirstName(String firstName);
+    Optional<Physiotherapist> findByUserId(Long userId);
 }
