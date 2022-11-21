af185c73fb6b785f032882cfc1d2d7cd6a6b3feb
DiegoDeLaCruz <u20201c334@upc.edu.pe>
2022-11-21 09:03:33 -0500

    feat(video sessions): added video session resource

---
+++ src/main/java/com/theraphy/backend/treatments/resource/VideoSessionResource.java
@@ -0,0 +1,13 @@
+package com.theraphy.backend.treatments.resource;
+
+import lombok.*;
+
+@Getter
+@Setter
+@With
+@NoArgsConstructor
+@AllArgsConstructor
+public class VideoSessionResource {
+    private Long id;
+    private String link;
+}
